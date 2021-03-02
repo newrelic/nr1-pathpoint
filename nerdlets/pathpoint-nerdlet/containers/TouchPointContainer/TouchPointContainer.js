3566d42e66c905c811557a51d091eec59ce49660
ddelgado-wigilabs <ddelgado@wigilabs.com>
2021-03-01 20:47:46 -0500

    Fix proptypes definitons

--- nerdlets/pathpoint-nerdlet/containers/TouchPointContainer/TouchPointContainer.js
+++ nerdlets/pathpoint-nerdlet/containers/TouchPointContainer/TouchPointContainer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 import TouchPoint from '../../components/TouchPoint/TouchPoint.js';
 
 /**
@@ -9,62 +9,85 @@ import TouchPoint from '../../components/TouchPoint/TouchPoint.js';
  * @class StepContainer
  * @extends {React.Component}
  */
+
 export default class TouchPointerContainer extends React.Component {
-
-
-    filterTouchpoints = (touchpoints, city) => {
-        let filter = [];
-        for (const touchpoint of touchpoints) {
-            for (const country of touchpoint.countrys) {
-                if (country === city) {
-                    filter.push(touchpoint);
-                }
-            }
+  filterTouchpoints = (touchpoints, city) => {
+    const filter = [];
+    for (const touchpoint of touchpoints) {
+      for (const country of touchpoint.countrys) {
+        if (country === city) {
+          filter.push(touchpoint);
         }
-        filter.sort(function (a, b) {
-            if (a.error > b.error) {
-                return -1;
-            }
-            if (a.error < b.error) {
-                return 1;
-            }
-            return 0;
-        });
-
-        return filter;
+      }
     }
+    filter.sort(function (a, b) {
+      if (a.error > b.error) {
+        return -1;
+      }
+      if (a.error < b.error) {
+        return 1;
+      }
+      return 0;
+    });
+    return filter;
+  };
 
-    render() {
-        let { touchpoints, city, colors, iconFireStatus, checkAllStatus, iconSixthSenseStatus, element, handleChange, visible,
-            idVisible, renderProps, openModalParent, updateTouchpointOnOff } = this.props;
-        let touchpointsFilter = this.filterTouchpoints(touchpoints, city);
-        return (
-            <>
-                {touchpointsFilter.map((touchpoint) => {
-                    return (
-                        <TouchPoint
-                            handleChange={handleChange}
-                            idVisible={idVisible}
-                            visible={visible}
-                            renderProps={renderProps}
-                            key={touchpoint.index}
-                            touchpoint={touchpoint}
-                            city={city}
-                            colors={colors}
-                            iconFireStatus={iconFireStatus}
-                            checkAllStatus={checkAllStatus}
-                            iconSixthSenseStatus={iconSixthSenseStatus}
-                            element={element}
-                            openModalParent={openModalParent}
-                            updateTouchpointOnOff={updateTouchpointOnOff}
-                        />
-                    )
-                })}
-            </>
-        );
-    }
+  render() {
+    const {
+      touchpoints,
+      city,
+      colors,
+      iconFireStatus,
+      checkAllStatus,
+      iconSixthSenseStatus,
+      element,
+      handleChange,
+      visible,
+      idVisible,
+      renderProps,
+      openModalParent,
+      updateTouchpointOnOff
+    } = this.props;
+    const touchpointsFilter = this.filterTouchpoints(touchpoints, city);
+    return (
+      <>
+        {touchpointsFilter.map(touchpoint => {
+          return (
+            <TouchPoint
+              handleChange={handleChange}
+              idVisible={idVisible}
+              visible={visible}
+              renderProps={renderProps}
+              key={touchpoint.index}
+              touchpoint={touchpoint}
+              city={city}
+              colors={colors}
+              iconFireStatus={iconFireStatus}
+              checkAllStatus={checkAllStatus}
+              iconSixthSenseStatus={iconSixthSenseStatus}
+              element={element}
+              openModalParent={openModalParent}
+              updateTouchpointOnOff={updateTouchpointOnOff}
+            />
+          );
+        })}
+      </>
+    );
+  }
 }
 
 TouchPointerContainer.propTypes = {
-    touchpoints: PropTypes.array.isRequired
+  touchpoints: PropTypes.array.isRequired,
+  city: PropTypes.number.isRequired,
+  colors: PropTypes.object.isRequired,
+  iconFireStatus: PropTypes.bool.isRequired,
+  checkAllStatus: PropTypes.bool.isRequired,
+  iconSixthSenseStatus: PropTypes.bool.isRequired,
+  element: PropTypes.object,
+  handleChange: PropTypes.func.isRequired,
+  visible: PropTypes.bool.isRequired,
+  idVisible: PropTypes.string.isRequired,
+  renderProps: PropTypes.func.isRequired,
+  openModalParent: PropTypes.func.isRequired,
+  updateTouchpointOnOff: PropTypes.func.isRequired
 }
