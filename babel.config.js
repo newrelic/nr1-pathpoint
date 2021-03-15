fc408bf0f401b46ddcb79f67ae3800904d31e1a9
Jeison <yfgaona@ucundinamarca.edu.co>
2021-03-15 13:21:21 -0500

    Test for async functions

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,19 @@
 // Copyright (c) 2014-present, Facebook, Inc. All rights reserved.
-
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-react'],
-  plugins: ['@babel/plugin-proposal-class-properties']
+  entry: ['@babel/polyfill']
 };
 module.exports = function(api) {
   const presets = ['@babel/preset-env', '@babel/preset-react'];
-  const plugins = ['@babel/plugin-proposal-class-properties'];
+  const plugins = [
+    [
+      '@babel/plugin-transform-runtime',
+      {
+        regenerator: true
+      }
+    ],
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-export-default-from'
+  ];
 
   /** this is just for minimal working purposes,
    * for testing larger applications it is
