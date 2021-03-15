f3ac14b33bf9a8fe9c2efa9dc1217b77717309a3
Jeison <yfgaona@ucundinamarca.edu.co>
2021-03-15 09:19:05 -0500

    Resolve problems

--- nerdlets/pathpoint-nerdlet/components/Component/index.js
+++ nerdlets/pathpoint-nerdlet/components/Component/index.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import { AccountsQuery } from 'nr1';
 
 class index extends Component {
   calculateSum = (a, b) => a + b;
+
+  recoveId = () => {
+    const { data } = AccountsQuery.query();
+    return data[0].id;
+  };
+
   render() {
-    return <div />;
+    return <div>Este es mi account id {this.recoveId()}</div>;
   }
 }
 
