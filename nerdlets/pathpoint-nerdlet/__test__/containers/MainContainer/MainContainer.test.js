c946a80f53a1e09157cc15939d4620694898643d
Jeison <yfgaona@ucundinamarca.edu.co>
2021-03-15 13:21:21 -0500

    Test for async functions

---
+++ nerdlets/pathpoint-nerdlet/__test__/containers/MainContainer/MainContainer.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import MainContainer from '../../../containers/MainContainer/MainContainer';
+
+jest.mock(
+  'nr1',
+  () => {
+    const AccountsQuery = {
+      query: jest.fn().mockReturnValue({
+        data: [{ id: 123 }]
+      })
+    };
+    const nerdlet = {
+      setConfig: jest.fn()
+    };
+    return {
+      AccountsQuery: AccountsQuery,
+      nerdlet: nerdlet
+    };
+  },
+  { virtual: true }
+);
+
+describe('<MainContainer/>', () => {
+  it('Render initial', () => {
+    const wrapper = shallow(<MainContainer />);
+  });
+});
