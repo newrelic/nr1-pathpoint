c4a53cd7914eaa13cd8bca4822cf9582ac3f393f
Jeison <yfgaona@ucundinamarca.edu.co>
2021-03-15 13:21:21 -0500

    Test for async functions

--- nerdlets/pathpoint-nerdlet/components/Component/index.js
+++ nerdlets/pathpoint-nerdlet/components/Component/index.js
@@ -2,15 +2,28 @@ import React, { Component } from 'react';
 import { AccountsQuery } from 'nr1';
 
 class index extends Component {
+  state = {
+    id: 0
+  };
+
+  componentDidMount() {
+    this.setAccountId();
+  }
+
+  setAccountId = async () => {
+    const idAccount = await this.recoveId();
+    this.setState({ id: idAccount });
+  };
+
   calculateSum = (a, b) => a + b;
 
-  recoveId = () => {
-    const { data } = AccountsQuery.query();
+  recoveId = async () => {
+    const { data } = await AccountsQuery.query();
     return data[0].id;
   };
 
   render() {
-    return <div>Este es mi account id {this.recoveId()}</div>;
+    return <div>Este es mi account id {this.state.id}</div>;
   }
 }
 
