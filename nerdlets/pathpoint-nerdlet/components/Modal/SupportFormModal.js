8cf236294106e7521e938648428d85bbedf83b6a
Germán <ggarcia@wigilabs.com>
2021-03-15 16:03:48 -0500

    Form Support

--- nerdlets/pathpoint-nerdlet/components/Modal/SupportFormModal.js
+++ nerdlets/pathpoint-nerdlet/components/Modal/SupportFormModal.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Formik, Form, Field } from 'formik';
-import * as Yup from 'yup';
-import { Button, FormControl } from 'react-bootstrap';
-import CustomSelect from './CustomSelect';
+import { Button, FormControl, Form, FormGroup } from 'react-bootstrap';
+import Select from 'react-select';
 
 // IMPORT ICONS
 import support from '../../images/support.svg';
@@ -11,37 +9,42 @@ import support from '../../images/support.svg';
 // IMPORT MESSAGES
 import messages from '../../config/messages.json';
 
-const contactSchema = Yup.object().shape({
-  name: Yup.string().required('Required'),
-  email: Yup.string()
-    .email()
-    .required('Required'),
-  message: Yup.string().required('Required')
-});
+let subject = '';
 
-const renderField = ({ onChange, placeholder, type, bsClass, margin }) => {
-  return (
-    <FormControl
-      style={{ marginTop: margin, borderRadius: 0 }}
-      bsClass={bsClass}
-      onChange={onChange}
-      type={type}
-      placeholder={placeholder}
-    />
-  );
+let name = '';
+
+let company = '';
+
+let account = '';
+
+let email = '';
+
+let phone = '';
+
+let message = '';
+
+const customStyles = {
+  container: provided => ({
+    ...provided,
+    width: '100%'
+  }),
+  control: styles => ({
+    ...styles,
+    width: '100%',
+    height: '30px !important',
+    minHeight: '30px !important'
+  }),
+  menu: provided => ({
+    ...provided,
+    width: '100%'
+  }),
+  indicatorsContainer: styles => ({
+    ...styles,
+    height: '30px !important'
+  })
 };
 
-const renderAreaField = ({ onChange, placeholder, bsClass, margin }) => {
-  return (
-    <FormControl
-      bsClass={bsClass}
-      onChange={onChange}
-      componentClass="textarea"
-      placeholder={placeholder}
-      style={{ marginTop: margin, borderRadius: 0 }}
-    />
-  );
-};
+const defaultOptions = defaultOptionsSelect();
 
 function HeaderSupportFormModal() {
   return (
@@ -55,157 +58,122 @@ function HeaderSupportFormModal() {
   );
 }
 
+function handleSubmit(event, values, handleSaveUpdateSupport) {
+  event.preventDefault();
+  const subject = values.subject;
+  values.subject = subject.label;
+  handleSaveUpdateSupport(values);
+}
+
 function BodySupportFormModal(props) {
   const { handleSaveUpdateSupport } = props;
-  const defaultOptions = defaultOptionsSelect();
   return (
     <div style={{ width: '350px' }}>
       <div className="modal4content" style={{ textAlign: 'justify' }}>
         {messages.configuration.support.message_support_01}
       </div>
-      <div className="modal4content space modalMaxHeightContent">
-        <Formik
-          initialValues={{
-            subject: '',
-            name: '',
-            company: '',
-            account: '',
-            email: '',
-            phone: '',
-            message: ''
-          }}
-          validationSchema={contactSchema}
-          onSubmit={
-            /* istanbul ignore next */ values => handleSaveUpdateSupport(values)
+      <div className="modal4content space">
+        <Form
+          onSubmit={e =>
+            handleSubmit(
+              e,
+              {
+                subject,
+                name,
+                company,
+                account,
+                email,
+                phone,
+                message
+              },
+              handleSaveUpdateSupport
+            )
           }
         >
-          {({ errors, touched, values, setFieldValue }) => (
-            <Form>
-              <Field
-                className="custom-select"
-                name="subject"
-                options={defaultOptions}
-                component={CustomSelect}
-                placeholder="Subject"
-              />
-              {errors.subject && touched.subject && (
-                <div style={{ color: 'red' }}>{errors.subject}</div>
-              )}
-
-              <Field
-                component={renderField}
-                type="text"
-                name="name"
-                onChange={
-                  /* istanbul ignore next */ event =>
-                    setFieldValue('name', event.target.value)
-                }
-                placeholder="Name"
-                bsClass="contact"
-                margin={errors.name && touched.name ? '5px' : '15px'}
-              />
-              {errors.name && touched.name && (
-                <div style={{ color: 'red' }}>{errors.name}</div>
-              )}
-
-              <Field
-                component={renderField}
-                type="company"
-                name="company"
-                onChange={
-                  /* istanbul ignore next */ event =>
-                    setFieldValue('company', event.target.value)
-                }
-                placeholder="Company"
-                bsClass="contact"
-                margin={errors.company && touched.company ? '5px' : '15px'}
-              />
-              {errors.company && touched.company && (
-                <div style={{ color: 'red' }}>{errors.company}</div>
-              )}
-
-              <Field
-                component={renderField}
-                type="account"
-                name="account"
-                onChange={
-                  /* istanbul ignore next */ event =>
-                    setFieldValue('account', event.target.value)
-                }
-                placeholder="Account Name"
-                bsClass="contact"
-                margin={errors.account && touched.account ? '5px' : '15px'}
-              />
-              {errors.account && touched.account && (
-                <div style={{ color: 'red' }}>{errors.account}</div>
-              )}
-
-              <Field
-                component={renderField}
-                type="email"
-                name="email"
-                onChange={
-                  /* istanbul ignore next */ event =>
-                    setFieldValue('email', event.target.value)
-                }
-                placeholder="Email"
-                bsClass="contact"
-                margin={errors.name && touched.name ? '5px' : '15px'}
-              />
-              {errors.email && touched.email && (
-                <div style={{ color: 'red' }}>{errors.email}</div>
-              )}
-
-              <Field
-                component={renderField}
-                type="number"
-                name="phone"
-                onChange={
-                  /* istanbul ignore next */ event =>
-                    setFieldValue('phone', event.target.value)
-                }
-                placeholder="Mobile"
-                bsClass="contact"
-                margin={errors.phone && touched.phone ? '5px' : '15px'}
-              />
-              {errors.phone && touched.phone && (
-                <div style={{ color: 'red' }}>{errors.phone}</div>
-              )}
-
-              <Field
-                component={renderAreaField}
-                name="message"
-                placeholder="Details"
-                bsClass="contact contact-detail"
-                value={values.content}
-                margin={errors.phone && touched.phone ? '5px' : '15px'}
-                onChange={
-                  /* istanbul ignore next */ event =>
-                    setFieldValue('message', event.target.value)
-                }
-              />
-              {errors.message && touched.message && (
-                <div style={{ color: 'red' }}>{errors.message}</div>
-              )}
-              <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-                <Button
-                  type="submit"
-                  variant="contained"
-                  color="primary"
-                  style={{
-                    background: '#0178bf',
-                    color: 'white',
-                    width: '45%',
-                    marginTop:
-                      Object.entries(errors).length > 0 ? '3px' : '20px'
-                  }}
-                >
-                  Submit
-                </Button>
-              </div>
-            </Form>
-          )}
-        </Formik>
+          <FormGroup controlId="formSubject">
+            <Select
+              name="Subject"
+              onChange={e => (subject = e)}
+              placeholder="Subject"
+              options={defaultOptions}
+              styles={customStyles}
+              theme={theme => ({
+                ...theme,
+                borderRadius: 0
+              })}
+            />
+          </FormGroup>
+          <FormGroup controlId="formName">
+            <FormControl
+              required
+              type="text"
+              placeholder="Name"
+              bsClass="support-modal-input-text"
+              onChange={e => (name = e.target.value)}
+            />
+          </FormGroup>
+          <FormGroup controlId="formCompany">
+            <FormControl
+              required
+              type="text"
+              placeholder="Company"
+              bsClass="support-modal-input-text"
+              onChange={e => (company = e.target.value)}
+            />
+          </FormGroup>
+          <FormGroup controlId="formAccount">
+            <FormControl
+              required
+              type="text"
+              placeholder="Account Name"
+              bsClass="support-modal-input-text"
+              onChange={e => (account = e.target.value)}
+            />
+          </FormGroup>
+          <FormGroup controlId="FormEmail">
+            <FormControl
+              required
+              type="email"
+              placeholder="Email"
+              bsClass="support-modal-input-text"
+              onChange={e => (email = e.target.value)}
+            />
+          </FormGroup>
+          <FormGroup controlId="formMobile">
+            <FormControl
+              required
+              pattern="[+0-9]{10}"
+              type="phone"
+              placeholder="Mobile"
+              bsClass="support-modal-input-text"
+              onChange={e => (phone = e.target.value)}
+            />
+          </FormGroup>
+          <FormGroup controlId="formDetails">
+            <FormControl
+              required
+              componentClass="textarea"
+              rows={5}
+              placeholder="Details"
+              bsClass="support-modal-input-text"
+              onChange={e => (message = e.target.value)}
+            />
+          </FormGroup>
+          <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              style={{
+                background: '#0178bf',
+                color: 'white',
+                width: '45%'
+              }}
+            >
+              Submit
+            </Button>
+          </div>
+        </Form>
       </div>
     </div>
   );
