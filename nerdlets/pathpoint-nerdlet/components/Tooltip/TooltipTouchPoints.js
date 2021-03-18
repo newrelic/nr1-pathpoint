b944abb7ccd42d1daa040e54af076b1fd0371bde
Daniel F. Delgado <ddelgado@wigilabs.com>
2021-03-18 15:43:25 -0500

    Added Tooltip

--- nerdlets/pathpoint-nerdlet/components/Tooltip/TooltipTouchPoints.js
+++ nerdlets/pathpoint-nerdlet/components/Tooltip/TooltipTouchPoints.js
@@ -10,55 +10,42 @@ import messages from '../../config/messages.json';
 const TooltipTouchPoints = () => {
   return (
     <div className="mainTooltip">
-      <Tooltip
-        id="toolTipTouchPoints"
-        classes={{ tooltip: 'MuiTooltip-tooltip-3' }}
-        disableFocusListener
-        title={
-          <>
-            <div>
-              <div style={{ width: '800px' }} className="tooltipTitle">
-                <div className="tooltipTitle__title">Touchpoints</div>
-              </div>
-              <div className="tooltipTitleLine" />
-              <div className="tooltipContent">
-                <div className="tooltipContent__content--two">
-                  <div className="tooltipContent__space">
-                    <p className="tooltipContent__text">
-                      {messages.tooltip.touchpoints.message_touchpoints_01}
-                    </p>
-                  </div>
-                  <div className="tooltipContent__space">
-                    <img width="300" height="200" src={imagen1} />
-                  </div>
-                </div>
-                <div className="tooltipContent__content--two">
-                  <div className="tooltipContent__space">
-                    <img src={imagen2} />
-                  </div>
-                  <div className="tooltipContent__space">
-                    <p className="tooltipContent__text">
-                      {messages.tooltip.touchpoints.message_touchpoints_02}
-                    </p>
-                  </div>
-                  <div className="tooltipContent__space">
-                    <img src={imagen3} />
-                  </div>
-                  <div className="tooltipContent__space">
-                    <p className="tooltipContent__text">
-                      {messages.tooltip.touchpoints.message_touchpoints_03}
-                    </p>
-                  </div>
-                </div>
-              </div>
+      <div>
+        <div className="tooltipTitle">
+          <div className="tooltipTitle__title">Touchpoints</div>
+        </div>
+        <div className="tooltipTitleLine" />
+        <div className="tooltipContent">
+          <div className="tooltipContent__content--two">
+            <div className="tooltipContent__space">
+              <p className="tooltipContent__text">
+                {messages.tooltip.touchpoints.message_touchpoints_01}
+              </p>
             </div>
-          </>
-        }
-      >
-        <a>
-          <img height="10px" src={iconInformation} />
-        </a>
-      </Tooltip>
+            <div className="tooltipContent__space">
+              <img width="300" height="200" src={imagen1} />
+            </div>
+          </div>
+          <div className="tooltipContent__content--two">
+            <div className="tooltipContent__space">
+              <img src={imagen2} />
+            </div>
+            <div className="tooltipContent__space">
+              <p className="tooltipContent__text">
+                {messages.tooltip.touchpoints.message_touchpoints_02}
+              </p>
+            </div>
+            <div className="tooltipContent__space">
+              <img src={imagen3} />
+            </div>
+            <div className="tooltipContent__space">
+              <p className="tooltipContent__text">
+                {messages.tooltip.touchpoints.message_touchpoints_03}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
