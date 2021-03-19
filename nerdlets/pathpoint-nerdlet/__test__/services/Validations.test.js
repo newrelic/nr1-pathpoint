dc534d6b18b49f629c0fb56a5b47f5fcabc5b639
Germán <ggarcia@wigilabs.com>
2021-03-18 19:43:26 -0500

    Validations unit test

---
+++ nerdlets/pathpoint-nerdlet/__test__/services/Validations.test.js
@@ -0,0 +1,163 @@
+import Validations from '../../services/Validations';
+
+jest.mock(
+  'nr1',
+  () => {
+    const NerdGraphQuery = {
+      query: jest.fn().mockImplementation(async ({ query }) => {
+        if (query.includes('BAD REQUEST')) {
+          const errors = [];
+          await new Promise((resolve, reject) => {
+            errors.push('Unexpected query');
+            return reject(errors);
+          });
+        } else if (query.includes('KPI')) {
+          let dataReturn = {};
+          await new Promise(resolve => {
+            dataReturn = {
+              data: {
+                actor: {
+                  measure_0: {
+                    nrql: {
+                      results: [{ value: 27 }]
+                    }
+                  },
+                  measure_1: {
+                    nrql: {
+                      results: [{ value: 1 }]
+                    }
+                  }
+                }
+              }
+            };
+            return resolve();
+          });
+          return dataReturn;
+        } else if (query.includes('measure')) {
+          let dataReturn = {};
+          await new Promise(resolve => {
+            dataReturn = {
+              data: {
+                actor: {
+                  measure_0: {
+                    nrql: {
+                      results: [{ count: 27 }]
+                    }
+                  },
+                  measure_1: {
+                    nrql: {
+                      results: [{ percentage: 27 }]
+                    }
+                  },
+                  measure_2: {
+                    nrql: {
+                      results: [{ score: 27 }]
+                    }
+                  },
+                  measure_3: {
+                    nrql: {
+                      results: [{ session: 27 }]
+                    }
+                  },
+                  measure_4: {
+                    nrql: {
+                      results: [
+                        {
+                          facet: 'abc123',
+                          count: 1,
+                          session: 'abc123'
+                        },
+                        {
+                          facet: 'cde345',
+                          count: 1,
+                          session: 'cde345'
+                        },
+                        {
+                          facet: 'fgh678',
+                          count: 1,
+                          session: 'fgh678'
+                        }
+                      ]
+                    }
+                  },
+                  measure_5: {
+                    nrql: {
+                      results: [
+                        {
+                          R1: 1,
+                          R2: 2
+                        }
+                      ]
+                    }
+                  }
+                }
+              }
+            };
+            return resolve();
+          });
+          return dataReturn;
+        } else if (query.includes('PathpointHistoricErrors')) {
+          let dataReturn = {};
+          await new Promise(resolve => {
+            dataReturn = {
+              data: {
+                actor: {
+                  account: {
+                    nrql: {
+                      results: [
+                        { count: 2, facet: [1, 1, 4] },
+                        { count: 0.3, facet: [1, 1, 4] }
+                      ]
+                    }
+                  }
+                }
+              }
+            };
+            return resolve();
+          });
+          return dataReturn;
+        } else if (query.includes('count query')) {
+          let dataReturn = {};
+          await new Promise(resolve => {
+            dataReturn = {
+              data: {
+                actor: {
+                  account: {
+                    nrql: {
+                      results: [{ count: 1080 }]
+                    }
+                  }
+                }
+              }
+            };
+            return resolve();
+          });
+          return dataReturn;
+        }
+      })
+    };
+
+    return {
+      NerdGraphQuery: NerdGraphQuery
+    };
+  },
+  { virtual: true }
+);
+
+describe('Validations class', () => {
+  let validations;
+
+  beforeEach(() => {
+    validations = new Validations();
+  });
+
+  describe('Function validateNrqlQuery', () => {
+    it('nrql', async () => {
+      const validateQuery = await validations.validateNrqlQuery('count query');
+      expect(validateQuery).toEqual({
+        data: [{ count: 1080 }],
+        errors: undefined
+      });
+    });
+  });
+});
