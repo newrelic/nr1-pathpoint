1b67d2a9f398461e21894fc8c809a691f0d2f809
Jeison <yfgaona@ucundinamarca.edu.co>
2021-03-19 18:19:11 -0500

    Touchpoint and tooltip unit test

--- nerdlets/pathpoint-nerdlet/__test__/components/Tooltip/Tooltips.test.js
+++ nerdlets/pathpoint-nerdlet/__test__/components/Tooltip/Tooltips.test.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 
+import ToolTip from '../../../components/Tooltip/Tooltip';
 import TooltipStages from '../../../components/Tooltip/TooltipStages';
 import TooltipSteps from '../../../components/Tooltip/TooltipSteps';
 import TooltipTouchPoints from '../../../components/Tooltip/TooltipTouchPoints';
 
 describe('Tooltips', () => {
+  it('<Tooltip/>', () => {
+    const tooltip = mount(
+      <ToolTip width="900" bottom>
+        <div />
+      </ToolTip>
+    );
+    expect(tooltip.length).toEqual(1);
+  });
+
+  it('Tooltip simulate setState show in true', () => {
+    const tooltip = shallow(
+      <ToolTip width="900" bottom>
+        <div />
+      </ToolTip>
+    );
+    tooltip.setState({ show: true });
+    expect(tooltip.length).toEqual(1);
+  });
+
   it('<TooltipSteps/>', () => {
     const tooltip = mount(<TooltipSteps />);
     expect(tooltip.length).toEqual(1);
