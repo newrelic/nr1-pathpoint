203325b129d3cfea5e843eb471ba326279aa0249
Daniel F. Delgado <ddelgado@wigilabs.com>
2021-03-24 16:05:16 -0500

    Add unit test for main container

--- nerdlets/pathpoint-nerdlet/containers/MainContainer/MainContainer.js
+++ nerdlets/pathpoint-nerdlet/containers/MainContainer/MainContainer.js
@@ -272,14 +272,10 @@ export default class MainContainer extends React.Component {
 
   ToggleHeaderButtons = target => {
     let previousIconCanaryStatus = null;
-    let previousIconGoutStatus = null;
-    let previousIconStartStatus = null;
     let previousIconFireStatus = null;
     this.setState(
       state => {
         previousIconCanaryStatus = state.iconCanaryStatus;
-        previousIconGoutStatus = state.iconGoutStatus;
-        previousIconStartStatus = state.iconStartStatus;
         previousIconFireStatus = state.iconFireStatus;
         return {
           iconCanaryStatus: false,
