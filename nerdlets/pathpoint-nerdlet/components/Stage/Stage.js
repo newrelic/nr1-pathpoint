8960d7a78c08cf04f7b2c02a6bd4c9572535be74
Rommel Samanez <rsamanez@gmail.com>
2021-08-16 10:18:32 -0500

    stage header and Arrow Update

--- nerdlets/pathpoint-nerdlet/components/Stage/Stage.js
+++ nerdlets/pathpoint-nerdlet/components/Stage/Stage.js
@@ -3,76 +3,51 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 // IMPORT IMAGES AND STATIC FILES
-import goutBlack from '../../images/goutBlack.svg';
+
 import DiamondBlue from '../../images/DiamondBlue.svg';
 import DiamondWhite from '../../images/Diamond.svg';
-import trafficIcon from '../../images/traffic.svg';
-import peopleIcon from '../../images/People.svg';
 import Arrow from '../Arrow/Arrow';
+import dangerHeart from '../../images/heart_red.svg';
+import goodHeart from '../../images/heart_green.svg';
+import warningHeart from '../../images/heart_yellow.svg';
 
-const Stage = ({
-  title,
-  valueCongestion,
-  percentageCongestion,
-  index,
-  goutQuantity,
-  status,
-  capacityPercentage,
-  totalCountStage,
-  onClickStage,
-  colors,
-  trafficIconType
-}) => {
-  const textLevelBar = index === 1 ? `APDEX` : ``;
-  const showHealth = index === 1;
-  const showTrafficIcon = false; //trafficIconType === 'traffic';
-  const colorCircle = calcCircleColor(status, colors);
-  const indicators = calculate(capacityPercentage);
+const Stage = ({ stage, onClickStage }) => {
+  const textLevelBar = `CONGESTION`;
+  let healthIcon = warningHeart;
+  if (stage.status_color === 'danger') {
+    healthIcon = dangerHeart;
+  }
+  if (stage.status_color === 'good') {
+    healthIcon = goodHeart;
+  }
+  const indicators = calculate(stage.capacity);
   return (
     <div className="stage">
       <div className="titleStage">
-        <div>
-          {`${title}`}
-          <div>
+        <div className="growFlex4">
+          <div className="contentHealth growFlex4">
+            <span className="growFlex4">
+              <img style={{ height: '25px' }} src={healthIcon} />
+              <span className="brakeTittle">{`${stage.title}`}</span>
+            </span>
             <span className="quantiyTitleStage">{`${transformK(
-              totalCountStage
+              stage.total_count
             )}`}</span>
-            <span className="separator" />
-
-            <span className="quantiyTitleStage">
-              {`${transformK(valueCongestion)}`}%
-            </span>
-            <span className="quantiyIcon">
-              <img
-                style={{ height: showTrafficIcon ? '11px' : '15px' }}
-                src={showTrafficIcon ? trafficIcon : peopleIcon}
-              />
-            </span>
           </div>
-        </div>
-        <div
-          style={{
-            alignItems: 'center',
-            marginRight: '5%',
-            visibility: 'hidden'
-          }}
-        >
-          <img src={goutBlack} height="15px" width="11px" />
-          <span className="goutTxt">{goutQuantity}</span>
         </div>
       </div>
       <div className="arrowStage">
         <div
           onClick={() => {
-            onClickStage(index);
+            onClickStage(stage.index);
           }}
           style={{ cursor: 'pointer' }}
         >
           <Arrow
             arrowWidth={250}
-            showHealth={showHealth}
-            lightColor={colorCircle}
-            latencyPercentage={percentageCongestion}
+            valueCongestion={stage.congestion.percentage}
+            percentageCongestion={stage.congestion.percentage}
+            trafficIconType={stage.trafficIconType}
             textLevelBar={textLevelBar}
           />
         </div>
@@ -88,33 +63,13 @@ const Stage = ({
           })}
         </div>
         <div className="percentText">
-          {index === 1
-            ? `Utilization: ${Math.round(capacityPercentage * 100) / 100}%`
-            : `${Math.round(capacityPercentage * 100) / 100}%`}
+          {stage.index === 1
+            ? `Infra Workload: ${Math.round(stage.capacity * 100) / 100}%`
+            : `${Math.round(stage.capacity * 100) / 100}%`}
         </div>
       </div>
     </div>
   );
-};
-
-const calcCircleColor = (status, colors) => {
-  const { danger, warning, good } = colors.status_color;
-  let result = [];
-  switch (status) {
-    case 'good':
-      result = `rgb(${good[0]},${good[1]},${good[2]})`;
-      break;
-    case 'warning':
-      result = `rgb(${warning[0]},${warning[1]},${warning[2]})`;
-      break;
-    case 'danger':
-      result = `rgb(${danger[0]},${danger[1]},${danger[2]})`;
-      break;
-    default:
-      result = `rgb(${warning[0]},${warning[1]},${warning[2]})`;
-      break;
-  }
-  return result;
 };
 
 const transformK = value => {
@@ -154,15 +109,6 @@ const calculate = level => {
 export default Stage;
 
 Stage.propTypes = {
-  title: PropTypes.string.isRequired,
-  percentageCongestion: PropTypes.number.isRequired,
-  valueCongestion: PropTypes.number.isRequired,
-  index: PropTypes.number.isRequired,
-  goutQuantity: PropTypes.number.isRequired,
-  status: PropTypes.string.isRequired,
-  capacityPercentage: PropTypes.number.isRequired,
-  totalCountStage: PropTypes.number.isRequired,
-  onClickStage: PropTypes.func.isRequired,
-  colors: PropTypes.object.isRequired,
-  trafficIconType: PropTypes.string.isRequired
+  stage: PropTypes.object.isRequired,
+  onClickStage: PropTypes.func.isRequired
 };
