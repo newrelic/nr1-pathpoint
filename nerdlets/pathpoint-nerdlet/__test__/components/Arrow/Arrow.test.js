2e2cf408ab5481005a24c0dd8a2435a28e08b20c
daniel alvarez <daniel.alvarez@nextisolutions.com>
2021-10-15 10:52:30 -0500

    actualizacion del los test unitarios y de componentes

--- nerdlets/pathpoint-nerdlet/__test__/components/Arrow/Arrow.test.js
+++ nerdlets/pathpoint-nerdlet/__test__/components/Arrow/Arrow.test.js
@@ -20,6 +20,57 @@ describe('<Arrow/>', () => {
     expect(step.length).toEqual(1);
   });
 
+  it('Arrow render percentageCongestion > 19', () => {
+    const step = mount(
+      <Arrow
+        arrowWidth={12}
+        valueCongestion={10}
+        percentageCongestion={21}
+        trafficIconType="traffic"
+        lightColor="blue"
+        latencyPercentage={100}
+        textLevelBar="anyText"
+        showHealth
+        arrowMode="NoFLOW"
+      />
+    );
+    expect(step.length).toEqual(1);
+  });
+
+  it('Arrow render percentageCongestion = 0', () => {
+    const step = mount(
+      <Arrow
+        arrowWidth={12}
+        valueCongestion={10}
+        percentageCongestion={0}
+        trafficIconType="traffic"
+        lightColor="blue"
+        latencyPercentage={100}
+        textLevelBar="anyText"
+        showHealth
+        arrowMode="FLOW"
+      />
+    );
+    expect(step.length).toEqual(1);
+  });
+
+  it('Arrow render trafficIconType = people', () => {
+    const step = mount(
+      <Arrow
+        arrowWidth={12}
+        valueCongestion={10}
+        percentageCongestion={21}
+        trafficIconType="people"
+        lightColor="blue"
+        latencyPercentage={100}
+        textLevelBar="anyText"
+        showHealth
+        arrowMode="FLOW"
+      />
+    );
+    expect(step.length).toEqual(1);
+  });
+
   it('Arrow render latency 0', () => {
     const step = mount(
       <Arrow
