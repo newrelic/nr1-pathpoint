1d83b39a736e385df73bd62b17bd707dc65549d1
daniel alvarez <daniel.alvarez@nextisolutions.com>
2021-10-28 13:09:35 -0500

    creacion de componente workload

---
+++ nerdlets/pathpoint-nerdlet/components/Workload/workload.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Workload = ({ workloadWidth, workloadValue }) => {};
+
+Workload.propTypes = {
+  workloadWidth: PropTypes.number.isRequired,
+  workloadValue: PropTypes.number.isRequired
+};
+
+export default Workload;
