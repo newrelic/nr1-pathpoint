b10fdbcdb795e8ef4581a63759b1c61f32311063
daniel alvarez <daniel.alvarez@nextisolutions.com>
2021-11-02 11:42:09 -0500

    actualizacion de tests

--- nerdlets/pathpoint-nerdlet/__test__/services/LogsConnector.test.js
+++ nerdlets/pathpoint-nerdlet/__test__/services/LogsConnector.test.js
@@ -13,17 +13,40 @@ describe('LogsConnector', () => {
     };
     logConnector.SendLog(datos);
     expect(logConnector.buffer.length).toEqual(1);
-    // eslint-disable-next-line no-console
-    console.log(logConnector);
   });
 
-  it('Function checkbuffer()', () => {
+  it('Function checkbuffer() with licenseKey', () => {
     logConnector.buffer = {
       pathpointID: '12587',
       mesagge: 'log enviado'
     };
-    logConnector.checkBuffer();
-    // eslint-disable-next-line no-console
-    console.log(logConnector);
+    logConnector.CheckBuffer();
+  });
+
+  it('Function checkbuffer() without licenseKey', () => {
+    logConnector.licenseKey = '';
+    logConnector.buffer = [
+      {
+        pathpointID: '12587',
+        mesagge: 'log enviado'
+      }
+    ];
+    logConnector.axiosInstance.post.then = jest.fn();
+    logConnector.CheckBuffer();
+    expect(logConnector.buffer.length).toEqual(0);
+  });
+
+  it('Function checkbuffer() without licenseKey and max buffer', () => {
+    logConnector.licenseKey = '';
+    for (let i = 0; i < 23000; i++) {
+      const obj = {
+        pathpointID: '12587',
+        mesagge: 'log enviado'
+      };
+      logConnector.buffer.push(obj);
+    }
+    logConnector.axiosInstance.post.then = jest.fn();
+    logConnector.CheckBuffer();
+    expect(logConnector.buffer.length).toEqual(0);
   });
 });
