69f85455d46bf2a0d4046f7be1725b423664e119
Daniel F. Delgado <ddelgado@wigilabs.com>
2021-11-05 17:48:52 -0500

    Added refactor of translate ajv errors at FileErrorFormModal

--- nerdlets/pathpoint-nerdlet/schemas/view.js
+++ nerdlets/pathpoint-nerdlet/schemas/view.js
@@ -208,8 +208,8 @@ export const CustomSchemaValidation = target => {
           counter += 1;
         } else {
           errors.push({
-            dataPath: `stages/${c}/steps/${i}/line`,
-            message: 'Line must be consecutive'
+            dataPath: `The stage '${stage.title}', in step at position ${i + 1}, `,
+            message: `the property 'line' must be consecutive`
           });
           counter += 1;
         }
@@ -220,8 +220,8 @@ export const CustomSchemaValidation = target => {
           if (id === value.id) {
             finded = true;
             errors.push({
-              dataPath: `stages/${c}/steps/${i}/values/${x}/id`,
-              message: 'This ID already exist'
+              dataPath: `The stage '${stage.title}', in step at position ${i + 1}, in substep at position ${x + 1}, `,
+              message: `the following id '${id}' already exist`
             });
           }
         });
@@ -231,15 +231,15 @@ export const CustomSchemaValidation = target => {
       });
     });
   });
-  target.stages.forEach((stage, i) => {
-    stage.touchpoints.forEach((touchpoint, c) => {
+  target.stages.forEach(stage => {
+    stage.touchpoints.forEach(touchpoint => {
       if (
         touchpoint.dashboard_url[0] !== false &&
         !touchpoint.dashboard_url[0].includes('https://')
       ) {
         errors.push({
-          dataPath: `stages/${i}/touchpoints/${c}/dashboard_url/0`,
-          message: `URL must match with new relic domain`
+          dataPath: `The stage '${stage.title}', in touchpoint ${touchpoint.title}, in dashboard_url at position 1, `,
+          message: `the URL must match with new relic domain`
         });
       }
       if (touchpoint.related_steps !== '') {
@@ -253,8 +253,8 @@ export const CustomSchemaValidation = target => {
           });
           if (!finded) {
             errors.push({
-              dataPath: `stages/${i}/touchpoints/${c}/related_steps`,
-              message: `ID doesn't exist`
+              dataPath: `The stage ${stage.title}, in touchpoint ${touchpoint.title}, in property 'related_steps, '`,
+              message: `the following id '${related}' doesn't exist`
             });
           }
         });
@@ -263,8 +263,10 @@ export const CustomSchemaValidation = target => {
       touchpoint.queries.forEach((query, x) => {
         if (!regex.test(query.type)) {
           errors.push({
-            dataPath: `stages/${i}/touchpoints/${c}/queries/${x}/type`,
-            message: `Type must only contains letters, numbers, dashes and underscores`
+            dataPath: `The stage ${stage.title}, in touchpoint ${
+              touchpoint.title
+            }, in query at position ${x + 1}, in property 'type', `,
+            message: `must only contains letters, numbers, dashes and underscores`
           });
         }
       });
@@ -277,8 +279,8 @@ export const CustomSchemaValidation = target => {
     currentVersion[1] !== targetVersion[1]
   ) {
     errors.push({
-      dataPath: `pathpointVersion`,
-      message: `The version you attemp upload is not compatible with the application`
+      dataPath: `Error in the property 'pathpointVersion', `,
+      message: `the version you attemp upload is not compatible with the application`
     });
   }
   if (errors.length === 0) {
