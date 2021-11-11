82029aae5eee209420056355f383ead586e15e7b
Daniel F. Delgado <ddelgado@wigilabs.com>
2021-11-11 10:22:05 -0500

    Added design for credentials modal and added exported function for translate AJV errors

--- nerdlets/pathpoint-nerdlet/components/Modal/GeneralConfigurationFormModal.js
+++ nerdlets/pathpoint-nerdlet/components/Modal/GeneralConfigurationFormModal.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button, Form } from 'react-bootstrap';
+import { Button, FormControl, Form, FormGroup } from 'react-bootstrap';
+import Select from '../Select/Select';
 
 // IMPORT ICONS
 import setup from '../../images/setup.svg';
@@ -21,74 +22,81 @@ function BodyGeneralConfigurationFormModal(props) {
   const { stageNameSelected, handleSaveUpdateGeneralConfiguration } = props;
   const datos = stageNameSelected.datos;
   return (
-    <div style={{ width: '600px', paddingTop: '20px' }}>
+    <div style={{ width: '400px', paddingTop: '20px' }}>
       <Form onSubmit={handleSaveUpdateGeneralConfiguration}>
-        {RenderForm(datos)}
-        <div
-          style={{
-            float: 'left',
-            margin: '20px 15px 0px 0px',
-            height: '50px'
-          }}
-        >
-          <span className="touchPointCheckbox">
-            <input id="checkbox_logs" name="checkbox_logs" type="Checkbox" />
-            <label className="checkboxLabel">Enable / Disable Logs</label>
-          </span>
-        </div>
-        <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-          <Button
-            type="submit"
-            variant="contained"
-            color="primary"
-            style={{ background: '#0178bf', color: 'white' }}
+        <FormGroup controlId="accountId">
+          <label style={{ margin: '0px' }}>Account ID</label>
+          <Select
+            name="subject"
+            options={[{ label: 'Select Account ID' }]}
+            handleOnChange={() => null}
+          />
+        </FormGroup>
+        <FormGroup controlId="ingestLicense">
+          <label style={{ margin: '0px' }}>Ingest License</label>
+          <FormControl
+            name="ingestLicense"
+            type="text"
+            placeholder="Type your ingest license"
+            bsClass="support-modal-input-text"
+            onChange={() => null}
+          />
+        </FormGroup>
+        <FormGroup controlId="userAPIKey">
+          <label style={{ margin: '0px' }}>User API Key</label>
+          <FormControl
+            name="userAPIKey"
+            type="text"
+            placeholder="Type your User API Key"
+            bsClass="support-modal-input-text"
+            onChange={() => null}
+          />
+          <div style={{ marginTop: '25px' }}>
+            <input type="checkbox" />
+            <label className="label-checkbox"> Pathpoint Loggin </label>
+          </div>
+          <div style={{ marginTop: '10px' }}>
+            <input type="checkbox" />
+            <label className="label-checkbox">
+              Flame Tools Script Updates{' '}
+            </label>
+          </div>
+          <div style={{ marginTop: '10px' }}>
+            <input type="checkbox" />
+            <label className="label-checkbox">Drop Tools Script Updates</label>
+          </div>
+          <div
+            style={{
+              marginTop: '20px',
+              display: 'flex',
+              justifyContent: 'flex-end'
+            }}
           >
-            Save / Update
-          </Button>
-        </div>
+            <Button
+              variant="outline-danger"
+              style={{
+                marginRight: '20px'
+              }}
+            >
+              Reset Credentials
+            </Button>
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              style={{
+                background: '#0178bf',
+                color: 'white'
+              }}
+            >
+              Save / Update
+            </Button>
+          </div>
+        </FormGroup>
       </Form>
     </div>
   );
 }
-
-const RenderForm = (datos) => {
-  return (
-    <div style={{ height: '40px' }}>
-      {renderField({
-        label: 'Session Count (Min)',
-        defaultValue: datos.sendingLogsEnableDisable,
-        id: 'min_count',
-        name: 'min_count'
-      })}
-    </div>
-  );
-};
-
-const renderField = ({ name, label, defaultValue, id, onChange }) => {
-  return (
-    <>
-      <input
-        id={id}
-        name={name}
-        type="text"
-        defaultValue={defaultValue}
-        onChange={/* istanbul ignore next */ e => onChange(e)}
-        className="inputText"
-        style={{
-          width: '60px',
-          border: '1px solid gray',
-          padding: '5px'
-        }}
-      />
-      <label
-        className="bodySubTitle"
-        style={{ marginLeft: '10px', fontSize: '14px' }}
-      >
-        {label}
-      </label>
-    </>
-  );
-};
 
 BodyGeneralConfigurationFormModal.propTypes = {
   stageNameSelected: PropTypes.object.isRequired,
